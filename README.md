be78d5409df5161224e04d056a58b09d8c02974e
elisalindstrom <elisalindstroem@gmail.com>
2026-03-09 09:44:21 +0100

    Initial med README

---
+++ README.md
@@ -0,0 +1,5 @@
+# DT211G Projektuppgift
+
+Webbapplikation som kombinerar data från olika API:er
+
+Av Elisa Lindström, 2026
---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html")
+            }
+        }
+    },
+    plugins: [
+        ViteImageOptimizer({
+            jpg: {
+                quality: 75
+            },
+            jpeg: {
+                quality: 75
+            },
+            webp: {
+                quality: 70
+            },
+            png: {
+                quality: 75
+            }
+        })
+    ]
+});
