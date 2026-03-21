efe44718a03e379e8fba0ffb6dbe3d8856252f85
elisalindstrom <elisalindstroem@gmail.com>
2026-03-21 18:45:42 +0100

    Utvecklat och delat upp spara funktioner (localstorage). Lagt till animationer

--- src/js/main.js
+++ src/js/main.js
@@ -9,11 +9,17 @@ const authorProfile = document.querySelector("#author-info-card");
 const upcomingSection = document.querySelector("#upcoming-books");
 const publishedSection = document.querySelector("#published-books");
 
+let upcomingBooks = [];
+let publishedBooks = [];
+
 displaySavedBooks();
 
 searchForm.addEventListener("submit", searchAuthor);
 
-// Läser av sökfältets input
+/**
+ * 
+ * @param {*} event 
+ */
 function searchAuthor(event) {
     event.preventDefault();
 
@@ -26,7 +32,11 @@ function searchAuthor(event) {
     fetchAuthor(authorValue);
 }
 
-// Hämta författare
+/**
+ * 
+ * @param {*} authorValue 
+ * @returns 
+ */
 async function fetchAuthor(authorValue) {
     try {
         const response = await fetch(`https://openlibrary.org/search/authors.json?q=${authorValue}`);
@@ -42,18 +52,22 @@ async function fetchAuthor(authorValue) {
     }
 }
 
-// Skriv ut om författare ej kan hittas
+/**
+ * 
+ */
 function displayAuthorNotFound() {
     const authorProfile = document.querySelector("#author-info-card");
     authorProfile.classList.remove("hidden");
 
     const authorNameElement = document.createElement("h2");
     authorNameElement.textContent = `Author not found`;
-
     authorProfile.appendChild(authorNameElement);
 }
 
-// Skriv ut författare
+/**
+ * 
+ * @param {*} author 
+ */
 function displayAuthor(author) {
     authorProfile.classList.remove("hidden");
 
@@ -79,22 +93,26 @@ function displayAuthor(author) {
         topWorkElement.textContent = `Top Work: ${author.top_work}`;
         authorProfile.appendChild(topWorkElement)
     }
-
     fetchBooks(authorName);
 }
 
-// Hämta böcker
+/**
+ * Hämtar böcker från Google Books API.
+ * @param {*} authorName 
+ * @returns 
+ */
 async function fetchBooks(authorName) {
     loader.classList.remove("hidden");
 
     try {
-        const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=inauthor:${authorName}&printType=books&maxResults=20&key=${API_KEY}`);
+        const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=inauthor:"${authorName}"&printType=books&maxResults=40&key=${API_KEY}`);
         const books = await response.json();
 
         if (books.items === false) {
             return;
         }
         filterBooks(books.items);
+        console.log(books.items);
     } catch (error) {
         console.error("Något gick fel" + error);
     } finally {
@@ -102,18 +120,25 @@ async function fetchBooks(authorName) {
     }
 }
 
-// Filtrera bort böcker utan titel, datum och omslag
+/**
+ * Filtrerar hämtade böcker så endast de med titel, datum och omslag skickas vidare.
+ * @param {*} books 
+ */
 function filterBooks(books) {
     const filteredBooks = books.filter(book => book.volumeInfo?.title && book.volumeInfo?.publishedDate && book.volumeInfo?.imageLinks);
 
     sortBooks(filteredBooks);
 }
 
-// Sortering av böcker
+/**
+ * Sorterar böcker utifrån dagens datum.
+ * @param {*} books 
+ */
 function sortBooks(books) {
     const today = new Date();
-    let upcomingBooks = [];
-    let publishedBooks = [];
+
+    upcomingBooks = [];
+    publishedBooks = [];
 
     books.forEach(book => {
         const publishedData = book.volumeInfo.publishedDate;
@@ -125,7 +150,6 @@ function sortBooks(books) {
             publishedBooks.push(book);
         }
     });
-
     // Sortera så senast publicerade böcker visas först
     publishedBooks.sort((a, b) =>
         new Date(b.volumeInfo.publishedDate) - new Date(a.volumeInfo.publishedDate));
@@ -134,18 +158,21 @@ function sortBooks(books) {
     displayPublishedBooks(publishedBooks);
 }
 
-
+/**
+ * Skriver ut kommande böcker
+ * @param {*} upcomingBooks 
+ */
 function displayUpcomingBooks(upcomingBooks) {
-    upcomingSection.classList.remove("hidden");
-
-    const sectionHeader = document.createElement("h2")
-    upcomingSection.appendChild(sectionHeader);
+    upcomingSection.innerHTML = "";
 
     if (upcomingBooks.length === 0) {
-        sectionHeader.textContent = `No Upcoming Releases Found`;
-    } else {
-        sectionHeader.textContent = `Upcoming Releases`;
+        return;
     }
+    upcomingSection.classList.remove("hidden");
+
+    const sectionHeader = document.createElement("h2");
+    upcomingSection.appendChild(sectionHeader);
+    sectionHeader.textContent = `Upcoming Releases`;
 
     upcomingBooks.forEach(book => {
         const card = createBookCard(book)
@@ -153,17 +180,21 @@ function displayUpcomingBooks(upcomingBooks) {
     })
 }
 
+/**
+ * Skriver ut publicerade böcker
+ * @param {*} publishedBooks 
+ */
 function displayPublishedBooks(publishedBooks) {
-    publishedSection.classList.remove("hidden");
-
-    const sectionHeader = document.createElement("h2")
-    publishedSection.appendChild(sectionHeader);
+    publishedSection.innerHTML = "";
 
     if (publishedBooks.length === 0) {
-        sectionHeader.textContent = `No Published Books Found`;
-    } else {
-        sectionHeader.textContent = `Published Books`;
+        return;
     }
+    publishedSection.classList.remove("hidden");
+    
+    const sectionHeader = document.createElement("h2");
+    publishedSection.appendChild(sectionHeader);
+    sectionHeader.textContent = `Published Books`;
 
     publishedBooks.forEach(book => {
         const card = createBookCard(book)
@@ -171,8 +202,15 @@ function displayPublishedBooks(publishedBooks) {
     })
 }
 
-// Skapa bokkort
+/**
+ * Skapar bokkort
+ * @param {*} book 
+ * @returns 
+ */
 function createBookCard(book) {
+    let savedBooks = JSON.parse(localStorage.getItem("bookData")) || [];
+    const bookExists = savedBooks.some(savedBook => savedBook.title === book.volumeInfo.title);
+
     const bookCard = document.createElement("div");
     bookCard.classList.add("book-card");
 
@@ -190,7 +228,7 @@ function createBookCard(book) {
 
     const date = document.createElement("p");
     const dateOnly = new Date(book.volumeInfo.publishedDate).toLocaleDateString();
-    date.textContent = `Releases ${dateOnly}`;
+    date.textContent = `Published ${dateOnly}`;
 
     const pageCount = document.createElement("p");
     const pages = book.volumeInfo.pageCount;
@@ -204,9 +242,16 @@ function createBookCard(book) {
     }
 
     const saveBtn = document.createElement("button");
-    saveBtn.textContent = "Want to Read";
     saveBtn.classList.add("btn", "save-btn");
+
     bookInfo.appendChild(saveBtn);
+
+    if (bookExists) {
+        saveBtn.textContent = "Saved";
+        saveBtn.disabled = true;
+    } else {
+        saveBtn.textContent = "Want to Read";
+    }
 
     const infoLink = document.createElement("a");
     infoLink.textContent = "View details";
@@ -215,32 +260,48 @@ function createBookCard(book) {
     bookInfo.appendChild(infoLink);
 
     saveBtn.addEventListener("click", () => {
-        const bookData = {
-            title: book.volumeInfo.title,
-            author: book.volumeInfo.authors,
-            date: book.volumeInfo.publishedDate,
-            link: book.volumeInfo.infoLink
-        };
+        saveBookCard(book);
 
-        const savedBooks = JSON.parse(localStorage.getItem("bookData")) || [];
-
-        const bookExists = savedBooks.some(savedBook => savedBook.title === bookData.title);
-
-        if (bookExists === false) {
-            savedBooks.push(bookData);
-        }
-
-        localStorage.setItem("bookData", JSON.stringify(savedBooks));
+        saveBtn.textContent = "Saved";
+        saveBtn.disabled = true;
 
         displaySavedBooks();
     })
     return bookCard;
 }
 
+/**
+ * 
+ * @param {*} book 
+ */
+function saveBookCard(book) {
+    let savedBooks = JSON.parse(localStorage.getItem("bookData")) || [];
+
+    const bookData = {
+        title: book.volumeInfo.title,
+        author: book.volumeInfo.authors,
+        date: book.volumeInfo.publishedDate,
+        link: book.volumeInfo.infoLink
+    };
+
+    savedBooks = JSON.parse(localStorage.getItem("bookData")) || [];
+    let bookExists = savedBooks.some(savedBook => savedBook.title === bookData.title);
+
+    if (bookExists === false) {
+        savedBooks.unshift(bookData);
+        localStorage.setItem("bookData", JSON.stringify(savedBooks));
+        displaySavedBooks();
+    }
+}
+
+/**
+ * 
+ * @returns 
+ */
 function displaySavedBooks() {
     savedBooksSection.innerHTML = "";
 
-    const savedBooks = JSON.parse(localStorage.getItem("bookData")) || [];
+    let savedBooks = JSON.parse(localStorage.getItem("bookData")) || [];
 
     if (savedBooks.length === 0) {
         savedBooksSection.classList.add("hidden");
@@ -253,45 +314,58 @@ function displaySavedBooks() {
     savedBooksSection.appendChild(savedBooksHeader);
 
     savedBooks.forEach(book => {
-        const savedCard = document.createElement("div");
-        savedCard.classList.add("saved-card-info");
-
-        const savedInfo = document.createElement("div");
-        savedInfo.classList.add("book-info");
-
-        const savedCardHeader = document.createElement("div");
-        savedCardHeader.classList.add("saved-card-header");
-
-        const title = document.createElement("p")
-        title.classList.add("bold");
-        title.textContent = book.title;
-
-        const authors = document.createElement("p")
-        authors.textContent = book.author.join(", ");
-
-        const publishedDate = document.createElement("p")
-        publishedDate.textContent = `Published ${book.date}`;
-
-        const infoLink = document.createElement("a");
-        infoLink.textContent = "View details";
-        infoLink.href = book.link;
-        infoLink.target = "_blank";
-
+        const savedCard = createSavedBookCard(book);
         savedBooksSection.appendChild(savedCard);
-        savedCard.append(savedCardHeader, savedInfo)
-        savedCardHeader.append(title)
-        savedInfo.append(authors, publishedDate, infoLink);
 
-        const deleteBtn = document.createElement("button");
-        deleteBtn.textContent = `Remove`;
-        deleteBtn.classList.add("btn", "delete-btn");
-        savedCardHeader.appendChild(deleteBtn);
-        deleteBtn.addEventListener("click", function () {
-            let updatedSavedBooks = JSON.parse(localStorage.getItem("bookData")) || [];
-            updatedSavedBooks = updatedSavedBooks.filter(savedBook => savedBook.title !== book.title);
-            localStorage.setItem("bookData", JSON.stringify(updatedSavedBooks))
+    })
+}
 
-            displaySavedBooks();
-        })
-    });
+/**
+ * Skapar kort för sparade böcker
+ * @returns 
+ */
+function createSavedBookCard(book) {
+    const savedCard = document.createElement("div");
+    savedCard.classList.add("saved-card-info");
+
+    const savedInfo = document.createElement("div");
+    savedInfo.classList.add("book-info");
+
+    const savedCardHeader = document.createElement("div");
+    savedCardHeader.classList.add("saved-card-header");
+
+    const title = document.createElement("p")
+    title.classList.add("bold");
+    title.textContent = book.title;
+
+    const authors = document.createElement("p")
+    authors.textContent = book.author.join(", ");
+
+    const publishedDate = document.createElement("p")
+    publishedDate.textContent = `Published ${book.date}`;
+
+    const infoLink = document.createElement("a");
+    infoLink.textContent = "View details";
+    infoLink.href = book.link;
+    infoLink.target = "_blank";
+
+    savedCard.append(savedCardHeader, savedInfo)
+    savedCardHeader.append(title)
+    savedInfo.append(authors, publishedDate, infoLink);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = `Remove`;
+    deleteBtn.classList.add("btn", "delete-btn");
+    savedCardHeader.appendChild(deleteBtn);
+
+    deleteBtn.addEventListener("click", function () {
+        let updatedSavedBooks = JSON.parse(localStorage.getItem("bookData")) || [];
+        updatedSavedBooks = updatedSavedBooks.filter(savedBook => savedBook.title !== book.title);
+        localStorage.setItem("bookData", JSON.stringify(updatedSavedBooks))
+
+        displaySavedBooks();
+        displayUpcomingBooks(upcomingBooks);
+        displayPublishedBooks(publishedBooks);
+    })
+    return savedCard;
 }
